4c067c0e1b35b0795de9eef995b7ce8380cfae44
Nick Kramer <krames12@gmail.com>
2020-10-20 14:20:04 -0400

    added key to list items

--- components/RestaurantList.js
+++ components/RestaurantList.js
@@ -6,8 +6,11 @@ const RestaurantList = () => {
       <h2>Restaurant List</h2>
       <ul className={styles.list}>
         {["Bill's Tavern", "Tap Houz", "Bubba Gump Shrimp"].map(
-          (name) => (
-            <li className={styles.item}>
+          (name, index) => (
+            <li 
+              className={styles.item}
+              key={index}
+            >
               <h3 className={styles.title}>{name}</h3>
               <p className={styles.location}>Woodward Ave.</p>
               <p className={styles.description}>Bacon ipsum dolor amet meatloaf beef excepteur, porchetta est veniam corned beef shoulder turducken tongue nisi. Deserunt bacon velit shankle turducken, shoulder laboris. Excepteur officia boudin tail elit tenderloin, enim anim labore doner jowl laboris.</p>
