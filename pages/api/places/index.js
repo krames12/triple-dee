e66a69eda5c80ab05a40f97e46075069c8da4ba5
Nick Kramer <krames12@gmail.com>
2020-10-24 22:25:21 -0400

    fixed location to be proper order

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -6,13 +6,12 @@ module.exports = async (request, response) => {
   const {lng, lat} = request.query;
 
   const placesKey = process.env.googlePlaces;
-  const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lng},${lat}&radius=1500&type=restaurant&key=${placesKey}`
+  const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=15000&type=restaurant&key=${placesKey}`
 
   await axios.get(placesUrl)
     .then( response => {
       response.json(response)
-      response.status(200).send({hi: "tesxt"});
-
+      response.status(200).send;
     })
     .catch( error => {
       response.status(400).send(error);
