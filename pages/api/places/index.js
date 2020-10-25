5e7b6dcb236412c5685a3199dabe2c3cf1459b14
Nick Kramer <krames12@gmail.com>
2020-10-24 22:47:43 -0400

    axios response works, api route doesnt

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,19 +1,23 @@
-const { default: RestaurantList } = require("../../../components/RestaurantList");
-
 const axios = require("axios");
+
+async function getPlacesNearMe (url) {
+  console.log("inside getPlaces");
+  await axios.get(url)
+  .then( response => response.data )
+  .catch( error => error)
+}
 
 module.exports = async (request, response) => {
   const {lng, lat} = request.query;
 
-  const placesKey = process.env.googlePlaces;
+  const placesKey = process.env.GOOGLE_PLACES;
   const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=15000&type=restaurant&key=${placesKey}`
 
-  await axios.get(placesUrl)
-    .then( response => {
-      response.json(response)
-      response.status(200).send;
-    })
-    .catch( error => {
-      response.status(400).send(error);
-    })
+  try {
+    const placesRequest = await getPlacesNearMe(placesUrl);
+    await console.log(placesRequest)
+    response.status(200).json(placesRequest)
+  } catch(error) {
+    response.status(400).json(error)
+  }
 }
