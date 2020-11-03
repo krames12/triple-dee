a669c7f6a971792b10fcb9023ea62b11708de5ed
Nick Kramer <krames12@gmail.com>
2020-11-03 11:35:13 -0500

    created getAllRestaurants endpoint

---
+++ graphql/restaurants.js
@@ -0,0 +1,39 @@
+import fetchHelper from "../helpers/fetchHelper";
+
+// get all restaurants
+export const getAllRestaurants = async (params) => {
+  const query = `query GetAllRestaurants($size: Int) {
+      allRestaurants(_size: $size) {
+        data {
+          _id
+          name
+          location
+          description
+          rating
+          season
+        }
+      }
+    }
+  `
+
+  const size = 15
+  const { data, error } = await fetchHelper(process.env.FAUNA_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SECRET}`,
+      'Content-type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables: { size },
+    }),
+  })
+
+  return {
+    allRestaurants: data.allRestaurants.data,
+    error,
+  }
+}
+
+// get all restaurants within a certain area
---
+++ pages/api/db/restaurants/index.js
@@ -0,0 +1,10 @@
+const restaurants = require("../../../../graphql/restaurants")
+
+module.exports = async (request, response) => {
+  const allRestaurants = await restaurants.getAllRestaurants();
+
+  // response.status(500).json(allRestaurants.errorMessage) :
+  response.status(200).json(allRestaurants);
+  // .then( ret => response.status(200).json(ret.data))
+  // .catch( error => response.status(500).json(error))
+}
