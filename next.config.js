37742d4f62ae4b408bcea9bdde7f457d7df00866
Nick Kramer <krames12@gmail.com>
2020-11-03 11:34:36 -0500

    adds fauna gql endpoint to secrets

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const ENV_VARS = {
     MAPBOX_SECRET: process.env.MAPBOX_SECRET,
     GOOGLE_PLACES: process.env.GOOGLE_KEY,
-    FAUNA_SECRET: process.env.FAUNA_SECRET
+    FAUNA_SECRET: process.env.FAUNA_SECRET,
+    FAUNA_ENDPOINT: process.env.FAUNA_ENDPOINT
 }
 
 module.exports = {
